8fd916a85a7cf19a2a6b19d0764f445e3e830061
Frank van Veelen <frank.van.veelen@tective.nl>
2025-02-01 17:24:27 +0000

    CORS?

--- rewild/rewild/settings.py
+++ rewild/rewild/settings.py
@@ -38,11 +38,17 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.gis',
+    "corsheaders",
     'rest_framework',
     'api',  # Our app
 ]
 
+CORS_ALLOWED_ORIGINS = [
+    "*",
+]
+
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
