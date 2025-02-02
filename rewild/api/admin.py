3a76aacd3b054143cd540ce9a42a04c5dd6500c0
Frank van Veelen <frank.van.veelen@tective.nl>
2025-02-02 09:31:25 +0100

    Push it, Bop it

--- rewild/api/admin.py
+++ rewild/api/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from .models import MissionType, MissionInstance, UserProfile, WildlifeSighting, ShopItem, UserPurchase, Location
+from .models import DoneMission, MissionType, MissionInstance, UserProfile, WildlifeSighting, ShopItem, UserPurchase, Location
 
 
 admin.site.register(MissionType)
 admin.site.register(MissionInstance)
+admin.site.register(DoneMission)
 admin.site.register(UserProfile)
 admin.site.register(WildlifeSighting)
 admin.site.register(ShopItem)
